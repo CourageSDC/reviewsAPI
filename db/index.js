9871fee9f25212baacf2bf351fa4de385935c0e8
Matthew Gilliam <moofus2000@gmail.com>
2022-08-01 02:33:06 -0700

    added support for dotenv to connect to postgres

--- db/index.js
+++ db/index.js
@@ -1,1 +1,14 @@
-const psql = require('pg');
+const { Pool, Client } = require('pg')
+require('dotenv').config();
+
+// pools will use environment variables
+// for connection information
+
+const pool = new Pool({
+  password: process.env.PSQLPASS
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
