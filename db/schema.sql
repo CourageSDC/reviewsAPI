b90b1bd992508e04789639392d7ce06ffc5dd1fd
Matthew Gilliam <moofus2000@gmail.com>
2022-08-01 21:55:08 -0700

    updated schema to hold more data

--- db/schema.sql
+++ db/schema.sql
@@ -11,15 +11,17 @@ CREATE DATABASE reviewsAPI;
 DROP TABLE IF EXISTS reviews CASCADE;
 
 CREATE TABLE reviews (
-  id INTEGER NOT NULL,
+  id SERIAL NOT NULL,
   product_id INTEGER NOT NULL,
   rating SMALLINT NOT NULL,
   recommended BOOLEAN NOT NULL DEFAULT 'false',
   reported BOOLEAN NOT NULL DEFAULT 'false',
+  summary TEXT NOT NULL,
   response TEXT NULL DEFAULT NULL,
   body TEXT NULL DEFAULT NULL,
   date DATE NOT NULL,
   name TEXT NOT NULL,
+  email VARCHAR(60),
   helpfulness INTEGER NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
@@ -31,9 +33,9 @@ CREATE TABLE reviews (
 DROP TABLE IF EXISTS photos;
 
 CREATE TABLE photos (
-  id INTEGER NOT NULL,
+  id SERIAL NOT NULL,
   review_id INTEGER NOT NULL,
-  url TEXT NOT NULL,
+  url VARCHAR(200) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (review_id) REFERENCES reviews (id)
 );
@@ -45,8 +47,8 @@ CREATE TABLE photos (
 DROP TABLE IF EXISTS characteristics CASCADE;
 
 CREATE TABLE characteristics (
-  id INTEGER NOT NULL,
-  characteristic TEXT NOT NULL,
+  id SMALLSERIAL NOT NULL,
+  characteristic VARCHAR(10) NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -57,11 +59,23 @@ CREATE TABLE characteristics (
 DROP TABLE IF EXISTS review_characteristics;
 
 CREATE TABLE review_characteristics (
-  id INTEGER NOT NULL,
+  id SERIAL NOT NULL,
   review_id INTEGER NOT NULL,
   characteristic_id INTEGER NOT NULL,
   rating SMALLINT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (review_id) REFERENCES reviews (id),
+  FOREIGN KEY (characteristic_id) REFERENCES characteristics (id)
+);
+
+-- ---
+-- Table 'review_characteristics'
+-- ---
+
+DROP TABLE IF EXISTS product_characteristics (
+  id SERIAL NOT NULL,
+  product_id INTEGER NOT NULL,
+  characteristic_id SMALLINT NOT NULL,
+  PRIMARY KEY (id),
   FOREIGN KEY (characteristic_id) REFERENCES characteristics (id)
 );
