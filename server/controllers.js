9ba779ccca4a9a368bd44dd8dc4f5aa920e33a29
Matthew Gilliam <moofus2000@gmail.com>
2022-08-02 03:13:09 -0700

    fixed server crash when making get request

--- server/controllers.js
+++ server/controllers.js
@@ -2,17 +2,18 @@ const db = require('../db');
 
 const getReviews = (product_id, count, page) => {
   let queryString = `SELECT
-    id, rating, summary, recommended, response, body, date, reviewer_name, helpfulness
+    review_id, rating, summary, recommended, response, body, date, reviewer_name, helpfulness
     FROM reviews
-    WHERE id > ${(page - 1) * count}
+    WHERE review_id > ${(page - 1) * count}
     AND product_id = ${product_id}
     AND reported = false
     LIMIT ${count}`;
   return db.query(queryString)
     .then((res) => {
-      return res.rows.map((review) => {
-        review.review_id = review.id;
-      })
+      return res.rows.map((review) => ({
+        ...review,
+        date: new Date(Number(1596080481467)).toISOString()
+      }));
     })
   };
 
